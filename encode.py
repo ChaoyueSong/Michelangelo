5f64e66342635d6e46daecf37df303fa01b368cf
Chaoyue Song <56154447+ChaoyueSong@users.noreply.github.com>
2025-03-16 19:11:15 +0800

    Update encode.py

--- encode.py
+++ encode.py
@@ -40,7 +40,7 @@ def reconstruction(args, model, bounds=(-1.25, -1.25, -1.25, 1.25, 1.25, 1.25), 
     
     return 0
 
-def load_model(ckpt_path="third_partys/Michelangelo/checkpoints/shapevae-256.ckpt"):
+def load_model(ckpt_path="third_partys/Michelangelo/checkpoints/aligned_shape_latents/shapevae-256.ckpt"):
     model_config = OmegaConf.load("third_partys/Michelangelo/configs/shapevae-256.yaml")
     if hasattr(model_config, "model"):
         model_config = model_config.model
